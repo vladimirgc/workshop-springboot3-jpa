dd421a9e8af98d124bdab3aa494e0a6fba250ce8
Vladimir Guimaraes de Carvalho <vguimaraes@inpi.gov.br>
2026-02-06 15:23:29 -0300

    CRUD Category - application-dev e application-prod

--- src/main/java/com/educandoweb/course/resources/CategoryResource.java
+++ src/main/java/com/educandoweb/course/resources/CategoryResource.java
@@ -1,21 +1,30 @@
 package com.educandoweb.course.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.educandoweb.course.entities.Category;
+import com.educandoweb.course.entities.User;
 import com.educandoweb.course.services.CategoryService;
 
 
 
 @RestController
 @RequestMapping(value = "/categories")
+@CrossOrigin(origins = "http://localhost:5173") // Vite
 public class CategoryResource {
 
 	@Autowired
@@ -34,5 +43,23 @@ public class CategoryResource {
 		return ResponseEntity.ok().body(obj);
 	}
 	
+	@PostMapping
+	public ResponseEntity<Category> insert(@RequestBody Category obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Category> update(@PathVariable Long id, @RequestBody Category obj){
+		obj = service.update(id, obj);
+		return ResponseEntity.ok(obj);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
 	
 }
--- src/main/java/com/educandoweb/course/services/CategoryService.java
+++ src/main/java/com/educandoweb/course/services/CategoryService.java
@@ -4,10 +4,16 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.educandoweb.course.entities.Category;
+import com.educandoweb.course.entities.User;
 import com.educandoweb.course.repositories.CategoryRepository;
+import com.educandoweb.course.services.exceptions.DatabaseException;
+import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class CategoryService {
@@ -25,4 +31,35 @@ public class CategoryService {
 		Optional<Category> obj = repository.findById(id);
 		return obj.get();
 	}
+	
+	public Category insert(Category obj) {
+		return repository.save(obj);
+	}
+	
+	public Category update(Long id, Category obj) {
+		try {
+		Category entity = repository.getReferenceById(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+		}catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+	
+	private void updateData(Category entity, Category obj) {
+		entity.setName(obj.getName());				
+	}
+	
+	public void delete(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		if (obj.isPresent()) {
+			try {
+				repository.deleteById(id);
+			} catch (DataIntegrityViolationException e) {
+				throw new DatabaseException(e.getMessage());
+			}
+		} else {
+			throw new ResourceNotFoundException(id);
+		}
+	} 
 }
